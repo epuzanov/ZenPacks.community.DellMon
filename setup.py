b38a10bea84843ab318581747b16b36bb754ac76
Egor Puzanov <epuzanov@gmx.de>
2012-11-19 20:17:57 +0100

    added LICENSE.txt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 NAME = "ZenPacks.community.DellMon"
 VERSION = "2.5.0"
 AUTHOR = "Egor Puzanov"
-LICENSE = ""
+LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.DellMon']
 INSTALL_REQUIRES = ['ZenPacks.community.deviceAdvDetail>=1.3']
